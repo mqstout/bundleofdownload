d62de7f65dd23343a292bdc38cd5933adefce2c1
mqstout <mqstout@users.noreply.github.com>
2016-01-23 02:21:02 -0500

    Update bundle.py

--- bundle.py
+++ bundle.py
@@ -3,6 +3,8 @@
 # Please, respect Bundle of Holding and download responsibly.
 # See the FAQ, https://bundleofholding.com/index/faq
 #
+# Developed with Python 3.5
+#
 # Prerequisites: splinter, requests
 #  [super user] pip install requests
 #  [super user] pip install splinter#
